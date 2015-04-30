12018515f64f8e8cdc78042e10709ea42306f8c9
TimdeJonge <t.dejonge@students.uu.nl>
2015-04-30 22:54:13 +0000

    Working Sieve of Eratosthenes

--- Test.py
+++ Test.py
@@ -1,11 +1,25 @@
 import sys
 import time
+import math
 
 # sys.argv is a list with the command-line arguments. sysv.arg[0] is the name of Python script
-print(sys.argv[1])
-
+intervalTop = int(sys.argv[1])
+target = str(sys.argv[2])
 T1 = time.perf_counter()
-
-
+primes = [2]
+print(intervalTop)
+print(target)
+for i in range(3, intervalTop):
+    isPrime = True
+    for j in primes:
+        if j>math.sqrt(i):
+            break
+        if i%j == 0:
+            isPrime = False
+            break
+    if (isPrime):
+        primes.append(i)
+    
 T2 = time.perf_counter()
 print('Time required', T2 - T1, 'sec.')
+print(len(primes))
