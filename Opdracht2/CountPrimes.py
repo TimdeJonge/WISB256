9e07729f550fcf93536750854de8f55fc930336c
TimdeJonge <t.dejonge@students.uu.nl>
2015-05-01 22:32:07 +0000

    Added CountPrimes, near complete, deleted bad file

---
+++ Opdracht2/CountPrimes.py
@@ -0,0 +1,34 @@
+import sys
+import math
+
+target = str(sys.argv[1])
+primes = [2]
+twinCount = 0
+twinConstant = 0.6601618
+for i in range(3, 10**7, 2):
+    isPrime = True
+    for j in primes:
+        if j>math.sqrt(i):
+            break
+        if i%j == 0:
+            isPrime = False
+            break
+    if (isPrime):
+        if i == primes[-1] + 2:
+            twinCount+=1
+        primes.append(i)
+
+fin = open(target, 'w')
+for prime in primes:
+    fin.write(str(prime) + "\n")
+    
+lastPrime = primes[-1]
+print("Largest Prime = " +str(lastPrime)) 
+print("--------------------------------")
+print("pi(N)         = " + str(len(primes))) 
+print("N\log(N)      = " + str(lastPrime/math.log(lastPrime)))
+print("ratio         = " + str(len(primes) / (lastPrime/math.log(lastPrime))))
+print("--------------------------------")
+print("pi_2(N)       = " + str(twinCount))
+print("2CN/log(N)^2  = " + str(lastPrime *2*twinConstant/(math.log(lastPrime)**2)))
+print("ratio         = " + str(twinCount / (lastPrime *2*twinConstant/(math.log(lastPrime)**2))))
