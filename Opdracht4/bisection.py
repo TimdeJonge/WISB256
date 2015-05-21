19f7f6b003711674381ff7957c3ca444657fd48e
TimdeJonge <t.dejonge@students.uu.nl>
2015-05-21 14:08:26 +0000

    fixed findAllRoots

--- Opdracht4/bisection.py
+++ Opdracht4/bisection.py
@@ -1,13 +1,32 @@
+import math
+
 def findroot(f,a,b,epsilon):
+    a = float(a)
+    b = float(b)
     m = (a + b)/2
-    print(m)
     if abs(a-b) < epsilon:
         return m
-    if f(a)*f(m) < 0:
+    if f(a)*f(m) <= 0:
         return findroot(f,a,m,epsilon)
-    elif f(b)*f(m) < 0:
+    elif f(b)*f(m) <= 0:
         return findroot(f,b,m,epsilon)
     else:
         return "No roots found."
-        
-print(findroot(lambda x: x*x, -1, 10, .001))
+
+def findAllRoots(f,a,b,epsilon):
+    h = abs(a-b)/epsilon
+    solutions = []
+    for i in range(int(math.floor(h)/2)):
+        answer = findroot(f,a+2*i*epsilon, a+(i+1)*2*epsilon,epsilon)
+        if answer != "No roots found.":
+            print answer
+            solutions.append(answer)
+    for i in solutions:
+        for j in solutions:
+            if i!=j:
+                if abs(i - j) < epsilon:
+                    print(i,j)
+                    solutions.remove(i)
+    return solutions
+    
+print(findAllRoots(lambda x: (x-1)*(x-2)*(x-3), 0, 100, .01))
