0c74d633e5d55b2e8e9704fd9730be3c7f621232
TimdeJonge <t.dejonge@students.uu.nl>
2015-05-28 14:09:18 +0000

    fixed Opdracht5 tot bonus

---
+++ Opdracht5/Vector.py
@@ -0,0 +1,41 @@
+import math
+
+class Vector(object):
+    def __init__(self, amount=0, values=[]):
+        self.amount = amount
+        if type(values)==list and values != []:
+            self.values = []
+            for value in values:
+                self.values.append(float(value))
+        elif type(values) == float:
+            self.values = [values for i in range(amount)]
+        else:
+            self.values = [0.0 for i in range(amount)]
+    
+    def __str__(self):
+        answer = '' 
+        for value in self.values:
+            answer += (str(value)+"\n")
+        return str(self.values)
+        
+    def lincomb(self, other, alpha, beta):
+        result = []
+        for i in range(self.amount):
+            result.append(self.values[i]*alpha + other.values[i]*beta)
+        return Vector(self.amount,result)
+    
+    def scalar(self, alpha):
+        return self.lincomb(self,alpha,0)
+        
+    def inner(self, other):
+        result = 0
+        for i in range(self.amount):
+            result += self.values[i]*other.values[i]
+        return result)
+    
+    def norm(self):
+        return math.sqrt(self.inner(self))
+
+u1 = Vector(3, [3.14,2,-5])
+u2 = Vector(3,1.0)
+print(u1.inner(u1))
